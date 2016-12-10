54a14a5fec6256585059d535f108ef5f7d776567
zhangwen <you@example.com>
2016-12-10 14:20:44 +0800

    资源文件加载

--- CarefreeHouseKeeping/app/src/main/java/micro/com/carefreehousekeeping/Activity/HomeActivity.java
+++ CarefreeHouseKeeping/app/src/main/java/micro/com/carefreehousekeeping/Activity/HomeActivity.java
@@ -1,15 +1,7 @@
 package micro.com.carefreehousekeeping.Activity;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import micro.com.carefreehousekeeping.R;
-
 public class HomeActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-    }
+
 }
