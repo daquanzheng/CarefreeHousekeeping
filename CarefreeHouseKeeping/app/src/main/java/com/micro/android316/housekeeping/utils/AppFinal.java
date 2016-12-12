2eb20d341f242093b3fbd5d7129581cba0323d9a
zhangwen <you@example.com>
2016-12-12 16:23:08 +0800

    分类，及其详情

---
+++ CarefreeHouseKeeping/app/src/main/java/com/micro/android316/housekeeping/utils/AppFinal.java
@@ -0,0 +1,13 @@
+package com.micro.android316.housekeeping.utils;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2016/12/10.
+ */
+
+public class AppFinal extends Application {
+    public static SaveInMemery memery=SaveInMemery.getMemoryCache(50);
+
+
+}
---
+++ CarefreeHouseKeeping/app/src/main/java/com/micro/android316/housekeeping/utils/SaleInMySQL.java
@@ -0,0 +1,8 @@
+package com.micro.android316.housekeeping.utils;
+
+/**
+ * Created by Administrator on 2016/12/10.
+ */
+
+public class SaleInMySQL {
+}
