2c26f22ebd1f0048d896be683b30ae816d4b5e4b
qingyuyefeng <97667024@qq.com>
2016-12-14 17:06:10 +0800

    订单板块的布局

---
+++ CarefreeHouseKeeping/app/src/main/java/com/micro/android316/housekeeping/model/Waitpay.java
@@ -0,0 +1,32 @@
+package com.micro.android316.housekeeping.model;
+
+/**
+ * Created by Administrator on 2016/12/13.
+ */
+public class Waitpay {
+    String range,time,place;
+
+    public String getRange() {
+        return range;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+}
