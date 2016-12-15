1fcd1bfe88d30c707bece3ad262394546d758de6
RoyalNeverGiveUp <767664872@qq.com>
2016-12-15 09:02:00 +0800

    no message

--- CarefreeHouseKeeping/app/src/main/java/com/micro/android316/housekeeping/activity/OnlineOnsultant.java
+++ CarefreeHouseKeeping/app/src/main/java/com/micro/android316/housekeeping/activity/OnlineOnsultant.java
@@ -10,11 +10,17 @@ import android.widget.TextView;
 
 import com.micro.android316.housekeeping.R;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 import com.micro.android316.housekeeping.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by Administrator on 2016/12/12.
@@ -56,8 +62,28 @@ public class OnlineOnsultant extends Activity {
 
         try {
             URL url=new URL(httpURL);
+            StringBuilder stringBuilder=new StringBuilder();
             HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.setConnectTimeout(5000);
+            httpURLConnection.connect();
+            if(httpURLConnection.getResponseCode()==200){
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream(),"utf-8"));
+                String str;
+                if((str=bufferedReader.readLine().toString())!=null){
+                   stringBuilder.append(str);
+                }
+                JSONObject jsonObject=new JSONObject(stringBuilder.toString());
+                if (jsonObject.getString("status").equals("1")){
+                    JSONArray jsonArray=jsonObject.getJSONArray("result");
+                    for(int i=0;i<jsonArray.length();i++){
+
+                    }
+                }
+            }
         } catch (IOException e) {
+            e.printStackTrace();
+        }catch (JSONException e){
             e.printStackTrace();
         }
     }
