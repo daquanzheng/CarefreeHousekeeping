23cc1b1921e3eb13a5e32c77cff4dd8640d35856
daquanZheng <584459122@qq.com>
2016-12-26 09:30:06 +0800

    打开登录界面

--- CarefreeHouseKeeping/app/src/main/java/com/micro/android316/housekeeping/activity/PersonalInformationActivity.java
+++ CarefreeHouseKeeping/app/src/main/java/com/micro/android316/housekeeping/activity/PersonalInformationActivity.java
@@ -1,7 +1,10 @@
 package com.micro.android316.housekeeping.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.micro.android316.housekeeping.R;
 import com.micro.android316.housekeeping.utils.Topbar;
@@ -11,17 +14,25 @@ import com.micro.android316.housekeeping.utils.Topbar;
  */
 public class PersonalInformationActivity extends Activity{
     Topbar topbar;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personal_information);
+        textView= (TextView) findViewById(R.id.personal_head_title);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(PersonalInformationActivity.this,Login.class);
+                startActivity(intent);
+            }
+        });
         topbar= (Topbar) findViewById(R.id.personal_information_topBar);
         topbar.setOnTopBarClickListener(new Topbar.topBarClickListener() {
             @Override
             public void leftClick() {
                 finish();
             }
-
             @Override
             public void rightClick() {
 
